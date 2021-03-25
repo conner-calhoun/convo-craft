44455b11207a0badda2d08edaab857f280dddb4f
Callium <conner610@gmail.com>
2021-03-25 14:52:05 -0600

    structure, remove useless files

---
+++ convo/dialogue.py
@@ -0,0 +1,48 @@
+from dearpygui import core, simple
+
+import json
+
+class Prompt:
+    '''
+    Prompt Class
+    '''
+
+    def draw_node(self):
+        # TODO: draw the imnode
+        pass
+
+    def to_json(self):
+        pass
+
+
+class Response:
+    '''
+    Response Class
+    '''
+
+    def draw_node(self):
+        # TODO: draw the imnode
+        pass
+
+    def to_json(self):
+        pass
+
+class DialogueTree:
+    '''
+    DialogueTree, keeps a list of nodes (Prompts and Responses)
+    '''
+    def __init__(self, name='default_name'):
+        self.name = name
+        self.nodes = []
+
+    def to_json(self):
+        return json.dumps({
+            'name': self.name
+        })
+
+class DialogueEditor:
+    '''
+    Dialogue Node Editor
+    '''
+    def __init__(self):
+        pass
